6ab3665d5de98404e601fdf204210f58db670827
耿科 <geng_ke@sina.com>
2013-07-13 21:15:47 +0800

    gradle支持aar包，所以移除了库源文件

--- autoupdate/src/main/java/com/lurencun/service/autoupdate/internal/ResponseCallback.java
+++
@@ -1,9 +0,0 @@
-package com.lurencun.service.autoupdate.internal;
-
-import com.lurencun.service.autoupdate.Version;
-
-
-public interface ResponseCallback {
-	void onFoundLatestVersion(Version version);
-	void onCurrentIsLatest();
-}
