6d374ddf69a1798a2eb50e4cf4085ff33b1e3bab
耿科 <geng_ke@sina.com>
2014-12-26 19:05:19 +0800

    完善分享

--- core/src/main/java/cn/nit/beauty/ui/ImageGalleryActivity.java
+++ core/src/main/java/cn/nit/beauty/ui/ImageGalleryActivity.java
@@ -25,8 +25,11 @@ import cn.nit.beauty.Helper;
 import cn.nit.beauty.Utils;
 import cn.nit.beauty.entity.User;
 import cn.nit.beauty.proxy.UserProxy;
+import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.onekeyshare.ShareContentCustomizeCallback;
+import cn.sharesdk.sina.weibo.SinaWeibo;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
@@ -47,6 +50,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.List;
 
 import cn.nit.beauty.R;
@@ -66,7 +70,6 @@ public class ImageGalleryActivity extends SherlockFragmentActivity {
     List<ImageInfo> imageInfoList;
     private GalleryAdapter mAdapter;
     private ViewPager mViewPager;
-    private ShareActionProvider actionProvider;
     private String objectKey, folder;
     private Message message;
     private Boolean autoPlay = false, isOriginal = false;
@@ -116,10 +119,6 @@ public class ImageGalleryActivity extends SherlockFragmentActivity {
                         startActivityForResult(intent, Utils.VIP);
                         Toast.makeText(ImageGalleryActivity.this, "有效期为" + currentUser.getExpiredDate() + "，请续费", Toast.LENGTH_SHORT).show();
                     }
-                }
-
-                if (actionProvider != null) {
-                    createShareIntent(Data.OSS_URL + imageInfoList.get(i).getUrl());
                 }
 
                 isOriginal = imageInfoList.get(i).isOriginal();
@@ -355,25 +354,6 @@ public class ImageGalleryActivity extends SherlockFragmentActivity {
         return super.dispatchKeyEvent(event);
     }
 
-    /**
-     * Creates a sharing {@link Intent}.
-     *
-     * @return The sharing intent.
-     */
-    private void createShareIntent(String key) {
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.setType("image/*");
-
-        File cacheFile = DiscCacheUtil.findInCache(key, ImageLoader.getInstance().getDiscCache());
-
-        if (cacheFile != null) {
-            Uri uri = Uri.fromFile(cacheFile);
-            shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
-            actionProvider.setShareIntent(shareIntent);
-        }
-    }
-
-
     @Override
     protected void onPause() {
         super.onPause();
@@ -443,30 +423,46 @@ public class ImageGalleryActivity extends SherlockFragmentActivity {
     }
 
     private void showShare() {
+        ImageInfo imageInfo = imageInfoList.get(mViewPager.getCurrentItem());
+
+        final String url = MobclickAgent.getConfigParams(this, "share_url") + "?p=" + URLEncoder.encode(imageInfo.getKey());
+
+        final String share_text = MobclickAgent.getConfigParams(this, "share_text");
+
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
+        oks.setDialogMode();
 
 // 分享时Notification的图标和文字
         oks.setNotification(R.drawable.icon, getString(R.string.app_name));
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle(getString(R.string.share));
+        oks.setTitle(getString(R.string.app_name));
         // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl(url);
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText(share_text);
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        File cacheFile = DiscCacheUtil.findInCache(Data.OSS_URL + imageInfo.getUrl(), ImageLoader.getInstance().getDiscCache());
+        oks.setImagePath(cacheFile.getAbsolutePath());//确保SDcard下面存在此张图片
         // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
+        oks.setUrl(url);
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
         oks.setComment("我是测试评论文本");
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
+        oks.setSiteUrl(url);
 
-// 启动分享GUI
+        oks.setShareContentCustomizeCallback(new ShareContentCustomizeCallback() {
+            @Override
+            public void onShare(Platform platform, Platform.ShareParams paramsToShare) {
+                if (SinaWeibo.NAME.equals(platform.getName())) {
+                    paramsToShare.setText(share_text + "→ →" + url + " @丽图-美腿套图");
+                }
+            }
+        });
+        // 启动分享GUI
         oks.show(this);
     }
 
